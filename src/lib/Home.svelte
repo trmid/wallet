c88aeb53b7062d2e0a070c47d45ab8c21544fbcc
Trevor Richard <40277611+trmid@users.noreply.github.com>
2024-10-16 19:46:17 -0400

    fix home app layout

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -2,35 +2,40 @@
   import { bookmarks, bookmarkInfo, appSrc } from './stores'
 </script>
 
-<div class="container">
-  {#each $bookmarks as bookmark}
-    <a
-      on:click|preventDefault={() => ($appSrc = bookmark)}
-      href={bookmark}
-      class="bookmark"
-      style:background-color={$bookmarkInfo[bookmark]?.backgroundColor}
-      style:color={$bookmarkInfo[bookmark]?.color}
-    >
-      <div class="header">
-        {#if $bookmarkInfo[bookmark]?.icon}
-          <img class="icon" src={$bookmarkInfo[bookmark].icon} alt="" />
-        {/if}
-        {#if $bookmarkInfo[bookmark]?.name}
-          <strong>{$bookmarkInfo[bookmark].name}</strong>
-        {/if}
-      </div>
-      <p>
-        {$bookmarkInfo[bookmark]?.description ?? bookmark}
-      </p>
-    </a>
-  {/each}
+<div class="wrapper">
+  <div class="container">
+    {#each $bookmarks as bookmark}
+      <a
+        on:click|preventDefault={() => ($appSrc = bookmark)}
+        href={bookmark}
+        class="bookmark"
+        style:background-color={$bookmarkInfo[bookmark]?.backgroundColor}
+        style:color={$bookmarkInfo[bookmark]?.color}
+      >
+        <div class="header">
+          {#if $bookmarkInfo[bookmark]?.icon}
+            <img class="icon" src={$bookmarkInfo[bookmark].icon} alt="" />
+          {/if}
+          {#if $bookmarkInfo[bookmark]?.name}
+            <strong>{$bookmarkInfo[bookmark].name}</strong>
+          {/if}
+        </div>
+        <p>
+          {$bookmarkInfo[bookmark]?.description ?? bookmark}
+        </p>
+      </a>
+    {/each}
+  </div>
 </div>
 
 <style>
-  .container {
+  .wrapper {
     position: absolute;
     inset: 0;
     overflow-y: auto;
+  }
+
+  .container {
     padding: 1rem;
     width: 1280px;
     max-width: 100vw;
@@ -54,7 +59,8 @@
     border-radius: 0.5rem;
     box-shadow: 3px 3px 7px #0008;
     overflow: hidden;
-    width: 220px;
+    width: 100%;
+    max-width: 220px;
     height: 150px;
   }
 
@@ -71,5 +77,27 @@
 
   .bookmark > p {
     font-size: small;
+  }
+
+  @media (max-width: 720px) {
+    .container {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-auto-rows: minmax(100px, auto);
+    }
+
+    .bookmark {
+      max-width: none;
+    }
+  }
+
+  @media (max-width: 400px) {
+    .container {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      flex-wrap: nowrap;
+    }
   }
 </style>
