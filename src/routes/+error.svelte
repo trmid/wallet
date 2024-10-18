9ba959e8460ad1d1340af1920805e0e0ffb80947
Trevor Richard <40277611+trmid@users.noreply.github.com>
2024-10-18 18:17:10 -0400

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    location.assign('/')
+  })
+</script>
+
+<div class="margins">
+  <h1>{$page.status}: {$page.error?.message}</h1>
+</div>
+
+<style>
+  .margins {
+    max-width: 1024px;
+    margin: 0 auto;
+    padding: 1rem;
+  }
+</style>
